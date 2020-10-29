01b19af08221af22450b70048cb75111abef6e3b
Arnab Gupta <arnabgupta91996@gmail.com>
2020-10-29 12:38:06 +0530

    test cases

--- src/test/java/com/example/razorpay/RazorpayApplicationTests.java
+++ src/test/java/com/example/razorpay/RazorpayApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.razorpay;
 
+import com.example.razorpay.controller.RazorpayController;
+import com.example.razorpay.services.MongoService;
+import com.example.razorpay.services.RazorpayService;
+import com.mongodb.MongoException;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RazorpayApplicationTests {
 
+	@Autowired
+	RazorpayService razorpayService;
+
+	@Autowired
+	MongoService mongoService;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() { }
+
+	@Test
+	void RazorpayServiceTest() throws Exception {
+		if(razorpayService==null)
+			throw new Exception();
+	}
+
+	@Test
+	void MongoServiceTest() throws MongoException {
+		if(mongoService==null||mongoService.mongoClient==null)
+			throw new MongoException("ERROR");
 	}
 
 }
